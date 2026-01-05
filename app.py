c6c2901d5616435f6954929bbf4c40be28792adc
fathia09 <fathia.abdou.mohamed@gmail.com>
2026-01-05 16:13:48 +0100

    modified:   app.py 	modified:   routes/admin.py 	modified:   static/css/admin/styleAdmin.css 	modified:   templates/admin/base_admin.html 	modified:   templates/admin/gestion_users.html

--- app.py
+++ app.py
@@ -49,20 +49,10 @@ def dashboard():
     from flask import session
     if session.get('role') == 'enseignant':
         return redirect(url_for('enseignant.dashboard'))
-<<<<<<< HEAD
-    elif session.get('role') == 'etudiant':
-        return redirect(url_for('etudiant.dashboard'))
-    elif session.get('role') == 'admin':
-        return redirect(url_for('admin.dashboard'))
-    else:
-        return redirect(url_for('index'))
-    
-=======
     elif session.get('role') == 'admin':
         return redirect(url_for('admin.dashboard'))
     return redirect(url_for('etudiant.dashboard'))
 
->>>>>>> origin/main
 # Gestion de la fermeture de la base de données
 app.teardown_appcontext(close_db)
 
